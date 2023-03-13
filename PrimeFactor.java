0edf87a152aadf3e446ea8a26caee8e6626abca2
Michaiah <126828023+CaiCanCode@users.noreply.github.com>
2023-03-13 11:16:44 -0700

    Add files via upload

---
+++ PrimeFactor.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class PrimeFactor {
+
+    public static boolean isPrime(long n) {
+
+        if(n == 2){
+            return true;
+        }else if(n%2 == 0) {
+            return false;
+        }
+
+        for(long i = 3; i <= Math.sqrt(n); i+=2) {
+
+            if(n%i==0) {
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+    public static void printPrimeFactors(long n, long j) {
+        if(n<0) {
+            System.out.println("-1");
+            n*=-1;
+        }
+        if(isPrime(n)){
+            System.out.println(n);
+            return;
+        }
+        for(long i = j; i<=n; i++) {
+            if(n%i==0 && isPrime(i)) {
+
+                System.out.println(i);
+                printPrimeFactors(n/i, i);
+                return;
+
+            }
+        }
+
+    }
+    
+    public static void main(String[] args) {
+
+        System.out.println("\nEnter an integer:");
+        Scanner scn = new Scanner(System.in);
+        // long n = scn.nextLong();
+        String input = scn.nextLine();
+        long n = Long.parseLong(input);
+        System.out.println("\nThe prime factors of " + n + " are:\n");
+        printPrimeFactors(n, 2);
+        scn.close();
+
+        System.out.println("\nFun fact: the biggest prime this program can store is 9 223 372 036 854 775 783");
+
+    }
+
+}
